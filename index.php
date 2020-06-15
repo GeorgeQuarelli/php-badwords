d4b5df6c84a079ff57a09a32edce3adb70c5c6a4
George Quarelli <quarelli.george@gmail.com>
2020-06-16 00:26:48 +0200

    php finish

--- index.php
+++ index.php
@@ -10,7 +10,17 @@
 
       $text = 'ciao come stai? sei proprio un puffo! si proprio un puffo!';
 
+      //determino le parole da sostituire
+      $bad_words = array("puffo");
 
+      //determino la censura
+      $censured = array("***");
+
+      //applico la cenura sulla frase
+      $text = str_ireplace($bad_words,$censured,$text);
+
+      //stampo la frase
+      echo $text;
 
     ?>
   </body>
