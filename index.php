1dd09e7a15d0f46686df58c8e08a09977a8f4b63
George Quarelli <quarelli.george@gmail.com>
2020-06-16 00:25:30 +0200

    inserisco la frase

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <h1>Badwords</h1>
+    <?php
 
-echo 'test';
+      $text = 'ciao come stai? sei proprio un puffo! si proprio un puffo!';
 
-?>
+
+
+    ?>
+  </body>
+</html>
