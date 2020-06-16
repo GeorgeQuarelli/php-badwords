ac8b32a0bde5b0d47f94708acf03944d6cf78cb1
George Quarelli <quarelli.george@gmail.com>
2020-06-16 10:16:16 +0200

    fix

--- index.php
+++ index.php
@@ -29,15 +29,12 @@
 
 <!-- CORREZIONE -->
 <?php
-$test = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+$text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-$lunghezza - strlen($text);
+$lunghezza = strlen($text);
 
 //determino le parole da sostituire passata in GET
 $bad_words = $_GET["Badwords"];
-
-
-$badword = $_GET['badword']
 
 $numero_sostituzioni = 0;
 
@@ -51,8 +48,8 @@ $testo_censurato = str_replace($badword, '***', $text, $numero_sostituzioni);
     <title>badwords</title>
   </head>
   <body>
-    <h2>testo non censurato</h2>
-    <p><?php echo $testo; ?></p>
+    <h2>testo non censurato:</h2>
+    <p><?php echo $text; ?></p>
     <h1>lungheezza testo: <?php echo $lunghezza; ?></h2>
     <h2>testo censurato:</h2>
     <p><?php echo $testo_censurato; ?></p>
