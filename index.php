126694f82c9237f4ad882996c7320154e1bb8cd2
George Quarelli <quarelli.george@gmail.com>
2020-06-16 10:11:54 +0200

    fix

--- index.php
+++ index.php
@@ -32,12 +32,10 @@
 $test = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 $lunghezza - strlen($text);
-=======
-      $text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-      //determino le parole da sostituire passata in GET
-      $bad_words = $_GET["Badwords"];
->>>>>>> master
+//determino le parole da sostituire passata in GET
+$bad_words = $_GET["Badwords"];
+
 
 $badword = $_GET['badword']
 
