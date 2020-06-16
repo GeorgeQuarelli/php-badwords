16d06fa8750c1e717eef7b7a7e47d21168014a7c
George Quarelli <quarelli.george@gmail.com>
2020-06-16 09:45:06 +0200

    fix

--- index.php
+++ index.php
@@ -10,8 +10,13 @@
 
       $text = 'ciao come stai? sei proprio un puffo! si proprio un puffo!';
 
+<<<<<<< d4b5df6c84a079ff57a09a32edce3adb70c5c6a4
       //determino le parole da sostituire
       $bad_words = array("puffo");
+=======
+      //determino le parole da sostituire passata in GET
+      $bad_words = $_GET["Badwords"];
+>>>>>>> fix
 
       //determino la censura
       $censured = array("***");
