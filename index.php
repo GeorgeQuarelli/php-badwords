07c1c0f007540a6a8e7ce1f56f0d2e6ee2f7137f
George Quarelli <quarelli.george@gmail.com>
2020-06-16 09:44:35 +0200

    fIX

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
     <h1>Badwords</h1>
     <?php
 
-      $text = 'ciao come stai? sei proprio un puffo! si proprio un puffo!';
+      $text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-      //determino le parole da sostituire
-      $bad_words = array("puffo");
+      //determino le parole da sostituire passata in GET
+      $bad_words = $_GETZ["Badwords"];
 
       //determino la censura
       $censured = array("***");
