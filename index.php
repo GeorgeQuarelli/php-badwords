20b579c0d44455e3d164dec1a9dc4bbf326b82e4
George Quarelli <quarelli.george@gmail.com>
2020-06-16 10:09:49 +0200

    correzione_ in classe

--- index.php
+++ index.php
@@ -1,32 +1,56 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
+<!-- <!DOCTYPE html> -->
+<!-- <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>PHP</title>
   </head>
   <body>
     <h1>Badwords</h1>
-    <?php
 
-      $text = 'ciao come stai? sei proprio un puffo! si proprio un puffo!';
 
-<<<<<<< d4b5df6c84a079ff57a09a32edce3adb70c5c6a4
-      //determino le parole da sostituire
-      $bad_words = array("puffo");
-=======
-      //determino le parole da sostituire passata in GET
-      $bad_words = $_GET["Badwords"];
->>>>>>> fix
+      // $text = 'ciao come stai? sei proprio un puffo! si proprio un puffo!';
+      //
+      // //determino le parole da sostituire
+      // $bad_words = array("puffo");
+      //
+      // //determino la censura
+      // $censured = array("***");
+      //
+      // //applico la cenura sulla frase
+      // $text = str_ireplace($bad_words,$censured,$text);
+      //
+      // //stampo la frase
+      // echo $text;
 
-      //determino la censura
-      $censured = array("***");
 
-      //applico la cenura sulla frase
-      $text = str_ireplace($bad_words,$censured,$text);
+//   </body>
+// </html> -->
 
-      //stampo la frase
-      echo $text;
+<!-- CORREZIONE -->
+<?php
+$test = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-    ?>
+$lunghezza - strlen($text);
+
+$badword = $_GET['badword']
+
+$numero_sostituzioni = -0;
+
+$testo_censurato = str_replace($badword, '***', $text, $numero_sostituzioni);
+
+ ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>badwords</title>
+  </head>
+  <body>
+    <h2>testo non censurato</h2>
+    <p><?php echo $testo; ?></p>
+    <h1>lungheezza testo: <?php echo $lunghezza; ?></h2>
+    <h2>testo censurato:</h2>
+    <p><?php echo $testo_censurato; ?></p>
+    <h2>Numero sostituzioni : <?php echo $numero_sostituzioni; ?></h2>
   </body>
 </html>
